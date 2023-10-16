ae700c3123d2e728d5dd6bb7d95d571560646b4b
mk3-v8 <des-7zon@hotmail.com>
2023-10-16 16:15:51 +0300

    update

--- src/Hypnonema.NUI/src/types/duiState.ts
+++
@@ -1,9 +0,0 @@
-export interface DuiState {
-  screenName: string;
-  isPaused: boolean;
-  startedAt: string;
-  duration: number;
-  currentSource: string;
-  repeat: boolean;
-  screen: Screen;
-}
